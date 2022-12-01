00800ea88a85111ca46c4f0375be37ce68054721
Fabio Zanardo <shadomhp@gmail.com>
2022-11-30 21:35:34 -0300

    Aula 18 - Estruturando Projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+
+
+import Container from './components/layout/Container'
 
 function App() {
   return (
-    <div className="App">
-     <p>Costs</p>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+        <li>Contato</li>
+      </div>
+
+      <Container customClass="min-height">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/company' element={<Company />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/newproject' element={<NewProject />} />
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </BrowserRouter>
   );
 }
 
