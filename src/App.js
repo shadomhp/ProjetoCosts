971402a6d2a966cd108edd2cb529d0bd5a91d8a4
Fabio Zanardo <shadomhp@gmail.com>
2022-12-01 21:38:58 -0300

    Aula 19 - Navbar e Footer

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
 import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
-
+import Projects from './components/pages/Projects'
 
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-        <li>Contato</li>
-      </div>
+      <Navbar />
 
       <Container customClass="min-height">
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/company' element={<Company />} />
+          <Route path='/projects' element={<Projects />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/newproject' element={<NewProject />} />
+          <Route path='/company' element={<Company />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </BrowserRouter>
   );
 }
